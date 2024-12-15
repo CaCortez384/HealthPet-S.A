f56039d06ff878140a06140bf95e78be02387338
Carlos Cortez <ca.cortez@duocuc.cl>
2024-12-15 16:52:01 -0300

    Forzar el uso de HTTPS en producción y mantener la lógica de productos bajo stock en las vistas

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Facades\View;
+use Illuminate\Support\Facades\URL;
 use App\Models\Producto;
 
 class AppServiceProvider extends ServiceProvider
@@ -17,23 +18,26 @@ class AppServiceProvider extends ServiceProvider
         //
     }
 
-    
+
 
 
     /**
      * Bootstrap any application services.
      */
     public function boot(): void
-    {
+    {   
+        if (env('APP_ENV') === 'production') {
+            URL::forceScheme('https');
+        }
 
-    // Pasar los productos bajo stock a todas las vistas que usan el layout
-    View::composer('components.app-layout', function ($view) {
-        // Obtener productos con stock por debajo del mínimo requerido
-        $productosBajoStock = Producto::whereColumn('stock_unidades', '<', 'cantidad_minima_requerida')->get();
+        // Pasar los productos bajo stock a todas las vistas que usan el layout
+        View::composer('components.app-layout', function ($view) {
+            // Obtener productos con stock por debajo del mínimo requerido
+            $productosBajoStock = Producto::whereColumn('stock_unidades', '<', 'cantidad_minima_requerida')->get();
 
-        // Pasar la variable a la vista
-        $view->with('productosBajoStock', $productosBajoStock);
-    });
+            // Pasar la variable a la vista
+            $view->with('productosBajoStock', $productosBajoStock);
+        });
 
 
         Paginator::useBootstrap(); // Asegúrate de que Bootstrap esté cargado en tu vista
